d65d300ef5a5fc7dedfe0c76579eb7b011e1163c
mogstuff <markmorgan.mcp@gmail.com>
2021-04-29 10:01:07 +0100

    feat: Arhitecture Requirement from Rubric First Pass
    meets majority of requirements in Architecture ofr Rubric. TODO README.md

--- src/server/index.js
+++ src/server/index.js
@@ -3,36 +3,32 @@ const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8081;
 
 dotenv.config();
 
-const express = require('express')
+const express = require('express');
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('dist'));
 
-app.use('cors');
-
-app.use(express.static('dist'))
-
-console.log(__dirname)
-
-
-
-
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')  ;
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log(`Example app listening on port ${port}!`);
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.send(mockAPIResponse);
 })
+
 
