879ea4224391ed177b27dd618c37e8e379730663
mogstuff <markmorgan.mcp@gmail.com>
2021-04-29 09:24:44 +0100

    feat: server packages installed

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,26 @@
 
+const path = require('path');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
+
 dotenv.config();
-var path = require('path')
+
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+app.use('cors');
+
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+
+
+
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
