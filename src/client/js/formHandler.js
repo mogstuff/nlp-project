52341aa732f58284a436220b102a2051ecd3373b
mogstuff <markmorgan.mcp@gmail.com>
2021-04-29 15:25:15 +0100

    feat: work in progress for submit hanlder

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,11 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('url').value
+    Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    console.log("::: Form Submitted :::");    
+
 }
 
 export { handleSubmit }
