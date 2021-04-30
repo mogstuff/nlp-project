0c513e053f1dec918aef1908be88f639113cd4ef
mogstuff <markmorgan.mcp@gmail.com>
2021-04-30 12:11:52 +0100

    feat: builds successfully with SASS

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,14 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-alert("I EXIST")
-console.log("CHANGE!!");
+//alert("I EXIST")
+//console.log("CHANGE!!");
 
-export { checkForName , handleSubmit }
+import "./styles/base.scss"
+import "./styles/footer.scss"
+import "./styles/form.scss"
+import "./styles/header.scss"
+import "./styles/resets.scss"
+
+
+export { checkForName , handleSubmit } 
