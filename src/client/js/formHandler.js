e85f504e1df4060c68400cb1f08d58beffa17e27
mogstuff <markmorgan.mcp@gmail.com>
2021-05-05 12:56:32 +0100

    feat: call api and update ui
    get data from api and update UI, also added preloader

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,27 +1,18 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    // check what text was put into the form field
     let formText = document.getElementById('url').value
-    Client.checkForName(formText);
+    Client.checkForName(formText); 
 
-    console.log("::: Form Submitted :::");    
+    let data = { url: encodeURI(formText) };
 
-    let data = { url : encodeURI(formText) };
+  showPreloader();
 
-    console.log(data); 
-
-  postUrl('http://localhost:8081/postData', data).then(updateUI()).catch((error) => {
-    console.error(error);
-});
-
+    postUrl('http://localhost:8081/postData', data);
 }
 
 
-const postUrl = async(url = '', data = {}) => {
-
-    console.info('posting data to Api');
-    console.log(data);
+const postUrl = async (url = '', data = {}) => {   
 
     const response = await fetch(url, {
         method: 'POST',
@@ -31,30 +22,94 @@ const postUrl = async(url = '', data = {}) => {
         },
 
         body: JSON.stringify(data)
-
     });
 
     try {
+     
         const data = await response.json();
-        console.info('response from posting data to Api: ');
+        console.log('response from api');
         console.log(data);
-        return data;
+        updateUI(data);
+
+       return data;     
 
     } catch (error) {
         displayErrors(error);
         console.error('error posting data to API', error);
     }
-
 };
 
 
-function displayErrors(error){
+function displayErrors(error) {
     console.log(error);
 }
 
 
-const updateUI = async () => {
+const updateUI = async (result) => {
     console.log('update ui here');
+    console.log(result);
+
+    console.log('result.score_tag: ');
+    console.log(result.score_tag);
+
+    let results = document.getElementById('results');    
+    results.classList.remove('loader');
+    results.innerHTML = '';
+    results.scrollIntoView({behavior: "smooth"});
+
+    let heading = document.createElement('h3');
+    heading.innerText = 'Analysis Results';
+    results.appendChild(heading);
+
+      // score tag
+      let score_tag = document.createElement('div');
+      score_tag.innerHTML = '<span>Score Tag: </span>' + result.score_tag;
+      score_tag.classList.add('score_tag');
+      results.appendChild(score_tag);
+     
+    // Confidence
+    let confidence = document.createElement('div');
+    confidence.innerHTML = '<span>Confidence: </span>' + result.confidence;
+    confidence.classList.add('confidence');
+    results.appendChild(confidence);
+
+    // Subjectivity
+    let subjectivity = document.createElement('div');
+    subjectivity.innerHTML = '<span>Subjectivity: </span>' + result.subjectivity;
+    subjectivity.classList.add('subjectivity');
+    results.appendChild(subjectivity);
+
+
+    // agreement
+    let agreement = document.createElement('div');
+    agreement.innerHTML = '<span>Agreement: </span>' + result.agreement;
+    agreement.classList.add('agreement');
+    results.appendChild(agreement);
+   
+    // irony
+    let irony = document.createElement('div');
+    irony.innerHTML = '<span>Irony: </span>' + result.irony;
+    irony.classList.add('irony');
+    results.appendChild(irony);  
+  
+
+    document.getElementById('results-section').classList.add('roundGrayBorder');
+
+}
+
+function showPreloader(){
+  
+    let results = document.getElementById('results');    
+    results.innerHTML = '';
+
+    let loader = document.createElement('div');
+    loader.classList.add('loader');
+    results.appendChild(loader); 
+
+    let heading = document.createElement('div');
+    heading.innerHTML = '<p>.... processing data</p>';
+    results.appendChild(heading);
+
 }
 
 
--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,13 @@ const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
 const port = process.env.PORT || 8081;
 
 dotenv.config();
+
+const mcKey = process.env.MEANING_CLOUD_KEY;
 
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js')
@@ -16,9 +20,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-// CHANGE 
-//app.use(express.static('src/client'));
-//TO THIS
 app.use(express.static('dist'));
 
 console.log(__dirname);
@@ -37,6 +38,21 @@ app.get('/test', function (req, res) {
 })
 
 
-app.post('/postData', bodyParser.json(), function (req, res) {
-    res.send(mockAPIResponse);
+app.post('/postData', bodyParser.json(), async (req, res) => {
+  
+    let articleUrl = req.body.url;
+    let apiUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${mcKey}&lang=auto&url=${articleUrl}`;
+
+    const payLoad = await fetch(apiUrl, {
+        method: 'POST'
+    });
+
+    try {
+        const payLoadData = await payLoad.json();
+        res.send(payLoadData);
+    } catch (error) {
+        console.error("error retreiing data from Meaning Cloud API", error);
+    }
+
+
 })
